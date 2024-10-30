c972d39771a0ca9d59dc965c764b18ec2cb49d91
EternalProg <eternal.proglife@gmail.com>
2024-10-30 11:33:17 +0200

    Added custom hooks

---
+++ frontend/src/hooks/useAllProducts.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+
+const useAllProducts = () => {
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    fetch('http://localhost:9000/api/products')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setProducts(data); // Зберігаємо всі продукти
+        setIsLoading(false); // Завантаження завершено
+      })
+      .catch((error) => {
+        console.error('Error fetching products:', error);
+        setError(error);
+        setIsLoading(false);
+      });
+  }, []);
+
+  return { products, isLoading, error };
+};
+
+export default useAllProducts;
